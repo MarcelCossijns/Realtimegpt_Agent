0f31462d26a8b6009aa63970f43a6daae4ef50c7
Marcel Cossijns <marcel.cossijns@adesso.de>
2025-07-14 08:05:10 +0200

    .

--- custom_components/realtimegpt_agent/conversation.py
+++ custom_components/realtimegpt_agent/conversation.py
@@ -30,11 +30,13 @@ class RealtimeGPTAgent(ConversationEntity):
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to Home Assistant."""
+        _LOGGER.warning("async_added_to_hass (conversation.py")
         await super().async_added_to_hass()
         conversation.async_set_agent(self.hass, self.entry, self)
 
     async def async_will_remove_from_hass(self) -> None:
         """When entity will be removed from Home Assistant."""
+        _LOGGER.warning("async_will_remove_from_hass (conversation.py")
         conversation.async_unset_agent(self.hass, self.entry)
         await super().async_will_remove_from_hass()
 
@@ -69,6 +71,7 @@ async def async_setup_entry(
         async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up conversation entities."""
+    _LOGGER.warning("async_setup_entry (conversation.py")
     for subentry in config_entry.subentries.values():
         if subentry.subentry_type != "conversation":
             continue
